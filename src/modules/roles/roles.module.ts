7f0fa9605afeeb22a0c3d8bc9217a1228328c401
Marlon Torres <maodev22@gmail.com>
2024-09-07 10:20:41 -0500

    Fix conexion and seeding

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesService } from './roles.service';
+
 import { Role } from './role.entity';
+import { AssignedRole } from './assigned-role.entity';
+import { User } from '@app/modules/users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [
+    TypeOrmModule.forFeature([Role]),
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([AssignedRole])
+  ],
   providers: [RolesService],
   exports: [RolesService],
 })
